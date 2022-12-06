fbe403f863e643ad3b2a53158ec702ae6279dd0c
exploreranduin <a1249967801@163.com>
2022-12-06 22:19:05 +0800

    12.6 未完

---
+++ 语法基础课/第三章练习题/test.cpp
@@ -0,0 +1,55 @@
+// 708. 偶数
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    for (int i = 2; i <= 100; i += 2)
+//    {
+//        cout << i << endl;
+//    }
+//    return 0;
+//}
+
+// 709. 奇数
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    int x;
+//    cin >> x;
+//
+//    for (int i = 1; i <= x; i += 2)
+//    {
+//        cout << i << endl;
+//    }
+//
+//    return 0;
+//}
+
+// 712. 正数
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    double x;
+//    int cnt = 0;
+//
+//    for (int i = 0; i < 6; i++)
+//    {
+//        cin >> x;
+//        if (x > 0)
+//            cnt++;
+//    }
+//
+//    cout << cnt << " positive numbers" << endl;
+//    return 0;
+//}
