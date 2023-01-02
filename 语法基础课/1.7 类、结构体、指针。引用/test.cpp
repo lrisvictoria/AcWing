57071b072ce2ef7c4f501328e424db23a495d958
exploreranduin <a1249967801@163.com>
2023-01-02 22:13:57 +0800

    1.2 add

--- 语法基础课/1.7 类、结构体、指针。引用/test.cpp
+++ 语法基础课/1.7 类、结构体、指针。引用/test.cpp
@@ -49,54 +49,98 @@ using namespace std;
 // class 不写 public 会默认为 private
 // struct 则默认为 public
 
-struct Person
+//struct Person
+//{
+//	public:
+//		int age;
+//		int height;
+//		double money;
+//		string books[100];
+//
+//	public:
+//		string name;
+//
+//		void say()
+//		{
+//			cout << "hello" << name << endl;
+//		}
+//
+//		int get_age()
+//		{
+//			return age;
+//		}
+//
+//		void add_money(double x)
+//		{
+//			money += x;
+//		}
+//
+//		// 构造函数 - 名字和类 / 结构体名字相同
+//		// 没有类型
+//		// 可以构造多个
+//
+//		Person(int _age, int _height, double _money)
+//		{
+//			age = _age;
+//			height = _height;
+//			money = _money;
+//		}
+//
+//		// 这种写法更快
+//		Person(int _age, int _heght) : age(_age), height(_heght)
+//		{
+//
+//		}
+//};
+//
+//int main()
+//{
+//	Person(19, 180, 100.10);
+//	Person(17, 15);
+// 
+//	return 0;
+//}
+
+// y 氏链表
+
+#include <iostream>
+
+struct Node
 {
-	public:
-		int age;
-		int height;
-		double money;
-		string books[100];
+	int val;
+	Node* next;
 
-	public:
-		string name;
-
-		void say()
-		{
-			cout << "hello" << name << endl;
-		}
-
-		int get_age()
-		{
-			return age;
-		}
-
-		void add_money(double x)
-		{
-			money += x;
-		}
-
-		// 构造函数 - 名字和类 / 结构体名字相同
-		// 没有类型
-		// 可以构造多个
-
-		Person(int _age, int _height, double _money)
-		{
-			age = _age;
-			height = _height;
-			money = _money;
-		}
-
-		// 这种写法更快
-		Person(int _age, int _heght) : age(_age), height(_heght)
-		{
-
-		}
+	// 析构函数
+	Node(int _val) : val(_val), next(NULL) {}
 };
 
 int main()
 {
-	Person(19, 180, 100.10);
-	Person(17, 15);
- 
+	Node* p = new Node(1);
+
+	auto q = new Node(2);
+	auto o = new Node(3);
+	
+	p->next = q;
+	q->next = o;
+	Node* head = p;
+
+	// 头插
+	Node* n = new Node(4);
+	n->next = head;
+	head = n;
+
+	// 删除
+	// y 总说的删除，是遍历不到这个点就行
+	// 有没有被删无所谓
+	Node* del = head->next;
+	head->next = head->next->next;
+	delete del;
+
+	// 遍历链表
+	for (Node* i = head; i != nullptr; i = i->next)
+	{
+		cout << i->val << " ";
+	}
 	return 0;
 }
