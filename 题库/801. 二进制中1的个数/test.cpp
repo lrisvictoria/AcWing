dd9934d7012514755e8b70de8cd6e84e8620c6bb
exploreranduin <a1249967801@163.com>
2023-01-06 13:29:07 +0800

    1.6 lowbit

---
+++ 题库/801. 二进制中1的个数/test.cpp
@@ -0,0 +1,53 @@
+/*
+* 给定一个长度为 n 的数列，请你求出数列中每个数的二进制表示中 1 的个数。
+
+输入格式
+第一行包含整数 n。
+
+第二行包含 n 个整数，表示整个数列。
+
+输出格式
+共一行，包含 n 个整数，其中的第 i 个数表示数列中的第 i 个数的二进制表示中 1 的个数。
+
+数据范围
+1≤n≤100000,
+0≤数列中元素的值≤109
+输入样例：
+5
+1 2 3 4 5
+输出样例：
+1 1 2 1 2
+*/
+
+// lowbit 
+// 
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int lowbit(int x)
+{
+    return x & -x;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    while (n--) {
+        int x, res = 0;
+        cin >> x;
+        while (x) {
+            x -= lowbit(x);
+            res++;
+        }
+        cout << res << ' ';
+    }
+
+    return 0;
+}
